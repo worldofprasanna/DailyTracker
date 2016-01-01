b4b7cfeeb9b1683519ed142c594bc53eef44c404
Prasanna <prasannacse2005@gmail.com>
2016-01-01 12:55:37 +0530

    Refactor activity types

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 ActivityType.create([{name: 'IN-OFFICE', keystroke: 1},
                      {name: 'DEV-HUDDLE', keystroke: 2},
-                     {name: 'PAIRING', keystroke: 3},
-                     {name: 'MEETING', keystroke: 4},
-                     {name: 'TT', keystroke: 5},
-                     {name: 'NOTHING', keystroke: 6},
-                     {name: 'LEAVING-OFFICE', keystroke: 7}])
+                     {name: 'PET-PROJECT', keystroke: 3},
+                     {name: 'PAIRING', keystroke: 4},
+                     {name: 'MEETING', keystroke: 5},
+                     {name: 'TT', keystroke: 6},
+                     {name: 'NOTHING', keystroke: 7},
+                     {name: 'DEV-NEWS', keystroke: 8}])
 p "Created #{ActivityType.count} activity types"
