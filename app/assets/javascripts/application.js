3bd6b05be0b1c6569b11682d9471721b168b6646
Prasanna <prasannacse2005@gmail.com>
2016-01-16 11:39:25 +0530

    UI Alignment for Activity box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require activities_helper.js
 //= require_tree .
---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,4 @@
+Chartkick.options = {
+    height: "300px",
+    padding: "10px 10px 10px 10px"
+}
