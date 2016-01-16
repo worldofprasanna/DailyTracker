aade1e0e20a904001da095adf2afb7e9e9cd8837
Prasanna <prasannacse2005@gmail.com>
2016-01-16 12:04:52 +0530

    Add starter data for chart

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -9,4 +9,24 @@ module ActivitiesHelper
     activity.activity_start_time
   end
 
+  def draw_timeline_chart(time_line)
+    # binding.pry
+    if time_line.size == 0
+      chart = (timeline [['NO-DATA', '00:00'.to_time, '01:00'.to_time]])
+    elsif
+      chart = (timeline time_line)
+    end
+    chart
+  end
+
+  def draw_summary_chart(activity_duration)
+    if activity_duration.size == 0
+      start_data = {'NO-DATA' => 30}
+      chart = (pie_chart start_data)
+    elsif
+      chart = (pie_chart activity_duration)
+    end
+    chart
+  end
+
 end
